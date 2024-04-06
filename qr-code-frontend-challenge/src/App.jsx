3262decfebba3de20c7c77200c25da10a237e1e3
Akonkwa Ushindi Isaac <akonkwaushindi@gmail.com>
2024-04-06 10:07:43 +0200

    Some modification

--- qr-code-frontend-challenge/src/App.jsx
+++ qr-code-frontend-challenge/src/App.jsx
@@ -4,16 +4,16 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div className="container">
+      <main>
         <div className="disp">
           <img src={qrCode} alt="QR-Code" />
-          <h2>Improve your front-end skills by building projects</h2>
+          <h1>Improve your front-end skills by building projects</h1>
           <p>
             Scan the QR code to visit Frontend Mentor and take your coding
             skills to the next level
           </p>
         </div>
-      </div>
+      </main>
     </>
   );
 }
